23cf8d90d26e840ab0061c810d11c0efd95172aa
jana <jana.lyon@icrossing.co.uk>
2016-11-22 15:44:19 +0000

    Support block assets added

--- wp-content/themes/audio-active/template-parts/support_block.php
+++ wp-content/themes/audio-active/template-parts/support_block.php
@@ -3,7 +3,11 @@
 		<div class="support-block">
 			<h2>Supported by:</h2>
 
+			<img src="/wp-content/themes/audio-active/images/youthMakers_logo.jpg" alt="" width="" class="supported-by-images" />
+			<img src="/wp-content/themes/audio-active/images/prs_logo.jpg" alt="" width="" class="supported-by-images" />
+			<img src="/wp-content/themes/audio-active/images/artsCouncil_logo.jpg" alt="" width="" class="supported-by-images" />
 
+			<a class="btn btn-action" href="#">Support us <i class="fa fa-chevron-right"></i></a>
 		</div>
 	</div>
 	<div class="col-xs-12 col-md-6">
@@ -13,10 +17,11 @@
 				<div class="col-sm-8">
 					<p>Much of our work can only materialise as a result of the generous donations we have received from members of the public and businesses alike.
 					</p>
+					<a class="btn btn-action" href="#">Make a donation <i class="fa fa-chevron-right"></i></a>
 					<?php ?>
 				</div>
 				<div class="col-sm-4">
-
+					<img src="/wp-content/themes/audio-active/images/mydonate.gif" alt="" width="" class="donation-card-image" />
 				</div>
 			</div>
 		</div>
