5194e521f390db86897da6ab823eb3341da32e73
jana <jana.lyon@icrossing.co.uk>
2016-11-29 11:36:52 +0000

    Quote_block and support_block updated with links. Header updated with target blank on links

--- wp-content/themes/audio-active/template-parts/support_block.php
+++ wp-content/themes/audio-active/template-parts/support_block.php
@@ -7,13 +7,13 @@
 			</div>
 			<div class="row">
 				<div class="col-xs-8 col-sm-5 supported-by">
-					<img src="/wp-content/themes/audio-active/images/artsCouncil_logo.jpg" alt="" width="" class="supported-by-images" />
+					<a href="http://www.artscouncil.org.uk/"><img src="/wp-content/themes/audio-active/images/artsCouncil_logo.jpg" alt="Arts Council Logo" width="" class="supported-by-images" /></a>
 				</div>
 				<div class="col-xs-4 col-sm-2 supported-by">
-					<img src="/wp-content/themes/audio-active/images/prs_logo.jpg" alt="" width="" class="supported-by-images" />
+					<a href="http://www.prsformusic.com/Pages/default.aspx"><img src="/wp-content/themes/audio-active/images/prs_logo.jpg" alt="PRS Logo" width="" class="supported-by-images" /></a>
 				</div>
 				<div class="col-xs-8 col-sm-5 supported-by">
-					<img src="/wp-content/themes/audio-active/images/youthMakers_logo.jpg" alt="" width="" class="supported-by-images" />
+					<a href="https://www.youthmusic.org.uk/"><img src="/wp-content/themes/audio-active/images/youthMakers_logo.jpg" alt="Youth Makers Logo" width="" class="supported-by-images" /></a>
 				</div>
 			</div>
 			<a class="btn btn-action" href="#">Support us <i class="fa fa-chevron-right"></i></a>
@@ -25,10 +25,10 @@
 					<p>Much of our work can only materialise as a result of the generous donations we have received from members of the public and businesses alike.
 					</p>
 					<?php ?>
-					<a class="btn btn-action" href="#">Make a donation <i class="fa fa-chevron-right"></i></a>
+					<a class="btn btn-action" href="https://mydonate.bt.com/donation/donate.html?charity=audioactive">Make a donation <i class="fa fa-chevron-right"></i></a>
 				</div>
 				<div class="col-xs-12 col-sm-6 make-dontation">
-					<img src="/wp-content/themes/audio-active/images/mydonate.gif" alt="" width="" class="donation-card-image" />
+					<a href="https://mydonate.bt.com/donation/donate.html?charity=audioactive"><img src="/wp-content/themes/audio-active/images/mydonate.gif" alt="Donate to AudioActive" width="" class="donation-card-image" /></a>
 				</div>
 		</div>
 	</div>
