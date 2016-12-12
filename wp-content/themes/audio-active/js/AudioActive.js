f9f448f65199b0317fe88f15bed316171c70c7ae
Emmanouil.Lainas <Emmanouil.Lainas@icrossing.co.uk>
2016-12-12 16:24:48 +0000

    -fixed navbar -added our work arrow animations -aligned all content

--- wp-content/themes/audio-active/js/AudioActive.js
+++ wp-content/themes/audio-active/js/AudioActive.js
@@ -18,10 +18,29 @@
     var isSubMenuActivated = false;
     TweenMax.set('.drawer-dropdown-menu', {overflow: "hidden", height: 0});
     TweenMax.set('.drawer-dropdown > .drawer-dropdown-menu > .item > .drawer-menu-item', {marginLeft: "100%"});
-    $('.drawer-menu > .drawer-dropdown').off("hidden");
-    $('.drawer-menu > .drawer-dropdown').off("shown");
 
-    $('.drawer-menu > .drawer-dropdown').click(function () {
+
+    $( document ).ready(function() {
+        //console.log( "ready!" );
+        $('.drawer-menu > .drawer-dropdown').unbind();
+        //$('.drawer-menu > .drawer-dropdown').off("shown");
+    });
+
+    $('.drawer-menu > .drawer-dropdown > a').on("touchstart click", drawerState);
+
+    $( window ).resize(function () {
+        if($('.drawer-dropdown > .drawer-menu-item').hasClass("active-drawer")){
+            $('.drawer-dropdown > .drawer-menu-item').toggleClass("active-drawer");
+        }
+        TweenMax.set('.arrow-gap > .fa-chevron-down', {rotation: 0});
+        TweenMax.set('.drawer-dropdown-menu', {overflow: "hidden", height: 0});
+        TweenMax.set('.drawer-dropdown > .drawer-dropdown-menu > .item > .drawer-menu-item', {marginLeft: "100%"});
+        $('.drawer-nav').drawer('refresh');
+    //    document.activeElement.blur();
+        isSubMenuActivated = false;
+    });
+    function drawerState(e){
+        e.stopPropagation(); e.preventDefault();
         if(!isSubMenuActivated){
             $('.drawer-dropdown > .drawer-menu-item').toggleClass("active-drawer");
             if(window.innerWidth > 480) {
@@ -31,6 +50,8 @@
             }
             TweenMax.to('.arrow-gap > .fa-chevron-down', 0.5, {rotation: 180, onComplete: function () {
                 $('.drawer-nav').drawer('refresh');
+        //        document.activeElement.blur();
+
             }});
             TweenMax.staggerTo('.drawer-dropdown > .drawer-dropdown-menu > .item > .drawer-menu-item', 0.5,
                 {
@@ -38,10 +59,11 @@
                     ease: Power3.easeOut}, 0.1);
             isSubMenuActivated = true;
         } else {
-            $('.drawer-dropdown > .drawer-menu-item').toggleClass("active-drawer");
             TweenMax.set('.drawer-dropdown-menu', {display: "block"});
             TweenMax.to('.drawer-dropdown-menu', 0.5, {overflow: "hidden", height: 0, onComplete: function () {
+                $('.drawer-dropdown > .drawer-menu-item').toggleClass("active-drawer");
                 $('.drawer-nav').drawer('refresh');
+            //    document.activeElement.blur();
             }});
             TweenMax.to('.arrow-gap > .fa-chevron-down', 0.5, {rotation: 0});
             TweenMax.staggerTo('.drawer-dropdown > .drawer-dropdown-menu > .item > .drawer-menu-item', 0.5,
@@ -50,15 +72,5 @@
                     ease: Power3.easeOut}, -0.1);
             isSubMenuActivated = false;
         }
-    });
-    $( window ).resize(function () {
-        if($('.drawer-dropdown > .drawer-menu-item').hasClass("active-drawer")){
-            $('.drawer-dropdown > .drawer-menu-item').toggleClass("active-drawer");
-        }
-        TweenMax.set('.arrow-gap > .fa-chevron-down', {rotation: 0});
-        TweenMax.set('.drawer-dropdown-menu', {overflow: "hidden", height: 0});
-        TweenMax.set('.drawer-dropdown > .drawer-dropdown-menu > .item > .drawer-menu-item', {marginLeft: "100%"});
-        $('.drawer-nav').drawer('refresh');
-        isSubMenuActivated = false;
-    });
+    }
 })();
